565c6e2802ab010df4fe55c31762d2e7c7d6c9b7
sofiyakryu <63737183+sofiyakryu@users.noreply.github.com>
2020-08-04 16:36:20 +0200

    Half finished

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -10,15 +8,47 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+let post = {};
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
 
+app.post("/compose", function(req, res) {
+  post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
